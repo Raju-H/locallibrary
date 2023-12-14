fd2cc4cbc3f33e5d91f60d18cf26910daf2e8fc8
Raju <rajuhosseng@gmail.com>
2023-12-15 00:30:31 +0600

    first migrations

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("admin/", include("catalog.urls")),
+    path("catalog/", include("catalog.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
