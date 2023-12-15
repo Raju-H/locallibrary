a04f1835440bbbc1109bd3c8878242c1f0f73fd3
Raju <rajuhosseng@gmail.com>
2023-12-16 00:04:36 +0600

    updated admin with class admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
-admin.site.register(Language)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
